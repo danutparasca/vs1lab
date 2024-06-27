bbfc30c7e155fc190f6ef639445fadcca96dd5f2
danutparasca <danparasca89@gmail.com>
2024-06-27 14:58:39 +0200

    Reapply "Aufgabe 3 Done"
    This reverts commit 7294fea2cd763738723e8722c23d0171d3fb798d.

--- Aufgabe3/gta_v3/models/geotag-store.js
+++ Aufgabe3/gta_v3/models/geotag-store.js
@@ -1,5 +1,8 @@
 // File origin: VS1LAB A3
 
+const GeoTag = require("./geotag");
+const GeoTagExamples = require("./geotag-examples");
+
 /**
  * This script is a template for exercise VS1lab/Aufgabe3
  * Complete all TODOs in the code documentation.
@@ -24,8 +27,61 @@
  * - Keyword matching should include partial matches from name or hashtag fields. 
  */
 class InMemoryGeoTagStore{
+    #allAvailableGeoTags = [];
+    constructor() {
+        for (const element of GeoTagExamples.tagList) {
+            const tag = new GeoTag(element[0], element[1], element[2], element[3]);
+            this.#allAvailableGeoTags.push(tag);
+        }
+        console.log(this.#allAvailableGeoTags)
+    }
 
-    // TODO: ... your code here ...
+    addGeoTag(geoTag){
+        console.log("HIER", geoTag)
+        this.#allAvailableGeoTags.push(geoTag);
+    }
+
+    removeGeoTag(name){
+        this.#allAvailableGeoTags = this.#allAvailableGeoTags.filter(tag => tag.name !== name);
+    }
+
+    getNearbyGeoTags(geoTag, radius){
+        console.log("Radius:", radius);
+        console.log("GeoTag:", geoTag);
+        return this.#allAvailableGeoTags.filter(tag => {
+            const distance = this._calculateDistance(geoTag.latitude, geoTag.longitude, tag.latitude, tag.longitude);
+            console.log("Distance:", distance);
+            return distance <= radius;
+        });
+    }
+    
+    searchNearbyGeoTags(request, radius){
+        console.log("Request:", request)
+        const helperTag = new GeoTag;
+        helperTag.latitude = request.latitude;
+        helperTag.longitude = request.longitude;
+        const nerabyTags = this.getNearbyGeoTags(helperTag, radius);
+        console.log("nearbyTags:", nerabyTags)
+        const matchingTags = nerabyTags.filter(tag => {
+            const nameMatches = tag.name.toLowerCase().includes(request.keyword.toLowerCase());
+            const hashtagMatches = tag.hashtag.toLowerCase().includes(request.keyword.toLowerCase());
+            return nameMatches || hashtagMatches;
+        })
+        console.log("matchingTags:", matchingTags)
+        return matchingTags;
+    }
+
+    _calculateDistance(lat1, lon1, lat2, lon2) {
+        const dLat = this._deg2rad(lat2 - lat1);
+        const dLon = this._deg2rad(lon2 - lon1);
+        const a = Math.pow(Math.sin(dLat / 2.0), 2) + Math.pow(Math.sin(dLon / 2.0), 2) * Math.cos(this._deg2rad(lat1)) * Math.cos(this._deg2rad(lat2));
+        const dist = 6378.388 * 2.0 * Math.atan2(Math.sqrt(a), Math.sqrt(1.0 - a));
+        return dist;
+    }
+
+    _deg2rad(deg) {
+        return deg * (Math.PI / 180);
+    }
 
 }
 
